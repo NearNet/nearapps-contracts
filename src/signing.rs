1316eeb22c46e327e1cbe05c85b3eb09542d1d62
Thiago Machado <thiago@keyko.io>
2021-12-01 12:04:40 -0300

    add verification of prehashed msg

--- src/signing.rs
+++ src/signing.rs
@@ -7,6 +7,9 @@ use crate::ContractContract;
 pub use types::{PubKey, SecKey, Sha256, SignCompact};
 
 pub mod types {
+    use digest::consts::{U32, U64};
+    use digest::{BlockInput, FixedOutputDirty, Reset, Update};
+    use near_sdk::env;
 
     /// Private Key value.
     ///
@@ -23,7 +26,9 @@ pub mod types {
     /// Sha256 value.
     ///
     /// Has a total size of 32 bytes.
-    #[derive(near_sdk::serde::Serialize, near_sdk::serde::Deserialize, Clone, PartialEq, Debug)]
+    #[derive(
+        near_sdk::serde::Serialize, near_sdk::serde::Deserialize, Clone, PartialEq, Debug, Default,
+    )]
     #[serde(crate = "near_sdk::serde")]
     #[serde(transparent)]
     pub struct Sha256(
@@ -31,6 +36,47 @@ pub mod types {
         //
         pub [u8; 32],
     );
+
+    impl Sha256 {
+        pub fn hash_bytes(msg_bytes: &[u8]) -> Self {
+            let hash = env::sha256(msg_bytes);
+            let hash = hash.as_slice();
+            assert_eq!(hash.len(), 32);
+            let mut res = [0u8; 32];
+            res.copy_from_slice(hash);
+            Sha256(res)
+        }
+    }
+
+    // needed so this is a Digest
+    impl BlockInput for Sha256 {
+        type BlockSize = U64;
+    }
+
+    // needed so this is a Digest
+    // but this is not needed for sign verification
+    impl Update for Sha256 {
+        fn update(&mut self, _input: impl AsRef<[u8]>) {
+            unimplemented!();
+        }
+    }
+
+    // needed so this is a Digest
+    // but this is not needed for sign verification
+    impl Reset for Sha256 {
+        fn reset(&mut self) {
+            unimplemented!();
+        }
+    }
+
+    // needed so this is a Digest
+    impl FixedOutputDirty for Sha256 {
+        type OutputSize = U32;
+
+        fn finalize_into_dirty(&mut self, out: &mut digest::Output<Self>) {
+            out.copy_from_slice(&self.0);
+        }
+    }
 
     /// Public Key serialized in compressed form.  
     /// Instead of having both `x` and `y` values, only `x` is present,
@@ -108,8 +154,10 @@ impl Contract {
         let seckey = k256::SecretKey::from_bytes(&seckey.0).unwrap();
         let signing_key = k256::ecdsa::SigningKey::from(seckey);
         let mut sign: k256::ecdsa::Signature = {
-            use k256::ecdsa::signature::Signer;
-            signing_key.sign(msg.as_bytes())
+            use k256::ecdsa::signature::DigestSigner;
+            use sha2::Digest;
+            let digest = digest::Digest::chain(sha2::Sha256::new(), msg);
+            signing_key.try_sign_digest(digest).unwrap()
         };
         sign.normalize_s().unwrap();
         {
@@ -122,21 +170,63 @@ impl Contract {
     }
 
     /// Returns `true` if `pubkey` authenticates the
-    /// `sign` of the `sha256` hash of the `msg`.  
+    /// `sign` of the `msg_hash`.  
     /// Returns `false` otherwise.
     ///
     /// The `msg` is hashed using `sha256` and that is used
     /// to verify the signature's authenticity.
     pub fn ecdsa_secp256k1_verify(pubkey: PubKey, sign: SignCompact, msg: String) -> bool {
         let pubkey = k256::PublicKey::from_sec1_bytes(&pubkey.0).unwrap();
-        let verify_key = k256::ecdsa::VerifyingKey::from(pubkey);
+
+        let hashed_msg = {
+            use ecdsa::hazmat::FromDigest;
+            let hashed_msg = Sha256::hash_bytes(msg.as_bytes());
+            k256::Scalar::from_digest(hashed_msg)
+        };
+
         let sign = {
             use k256::ecdsa::signature::Signature;
             k256::ecdsa::Signature::from_bytes(&sign.0).unwrap()
         };
+
         {
-            use k256::ecdsa::signature::Verifier;
-            verify_key.verify(msg.as_bytes(), &sign).is_ok()
+            use ecdsa::hazmat::VerifyPrimitive;
+            pubkey
+                .as_affine()
+                .verify_prehashed(&hashed_msg, &sign)
+                .is_ok()
+        }
+    }
+
+    /// Returns `true` if `pubkey` authenticates the
+    /// `sign` of the `sha256` hash of the `msg`.  
+    /// Returns `false` otherwise.
+    ///
+    /// The `msg_hash` must be the result of a `sha256` of the msg,
+    /// and must have a total size of 32-bytes.
+    pub fn ecdsa_secp256k1_verify_hashed(
+        pubkey: PubKey,
+        sign: SignCompact,
+        hashed_msg: Sha256,
+    ) -> bool {
+        let pubkey = k256::PublicKey::from_sec1_bytes(&pubkey.0).unwrap();
+
+        let hashed_msg = {
+            use ecdsa::hazmat::FromDigest;
+            k256::Scalar::from_digest(hashed_msg)
+        };
+
+        let sign = {
+            use k256::ecdsa::signature::Signature;
+            k256::ecdsa::Signature::from_bytes(&sign.0).unwrap()
+        };
+
+        {
+            use ecdsa::hazmat::VerifyPrimitive;
+            pubkey
+                .as_affine()
+                .verify_prehashed(&hashed_msg, &sign)
+                .is_ok()
         }
     }
 
